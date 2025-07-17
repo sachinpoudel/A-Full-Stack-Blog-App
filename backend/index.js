4e57e06d319b653a4d826cad7529e02e49f0929f
sachinpoudel <poudelsachin009@gmail.com>
2025-07-17 22:10:21 +0545

    Your commit message here

--- backend/index.js
+++ backend/index.js
@@ -27,8 +27,8 @@ app.use(session({
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: true,
+    sameSite: 'None',
     maxAge: 24 * 60 * 60 * 1000,
     // 24 hours
     path: '/' 
--- backend/utlis/jwtAndCookie.js
+++ backend/utlis/jwtAndCookie.js
@@ -6,8 +6,8 @@ export const jwtAndCookie = async (res, userId) => {
   }, process.env.JWT_SECRET);
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
+    secure: true,
+    sameSite: 'None',
     maxAge: 24 * 60 * 60 * 1000,
     // 24,
     path: '/' // Important! Cookies won't work without a matching path
